a82d0aa2765ef8b843afc0ac4845719d88266a50
sinanalizz123-max <sinanalizz123@gmail.com>
2026-01-25 00:16:56 +0530

    Add visible calculator UI

--- app/src/main/kotlin/com/calculator/app/MainActivity.kt
+++ app/src/main/kotlin/com/calculator/app/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.calculator.app
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val display = findViewById<TextView>(R.id.display)
+        findViewById<Button>(R.id.btnTest).setOnClickListener {
+            display.text = "2"
+        }
     }
 }
