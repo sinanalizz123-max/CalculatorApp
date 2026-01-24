b42b14484b38086f6a79e671eb5902b32687ac9d
sinanalizz123-max <sinanalizz123@gmail.com>
2026-01-25 02:57:19 +0530

    Fix BigDecimal math + scientific display

--- app/src/main/kotlin/com/calculator/app/CalcEngine.kt
+++ app/src/main/kotlin/com/calculator/app/CalcEngine.kt
@@ -2,47 +2,21 @@ package com.calculator.app
 
 import java.math.BigDecimal
 import java.math.MathContext
-import java.math.RoundingMode
 
 object CalcEngine {
-    private val mc = MathContext(34, RoundingMode.HALF_UP)
+    private val mc = MathContext.DECIMAL128
+    var value = BigDecimal.ZERO
 
-    fun eval(expr: String): BigDecimal {
-        return Parser(expr).parse()
+    fun set(v: BigDecimal) {
+        value = v
     }
 
-    private class Parser(val s: String) {
-        var i = 0
+    fun add(v: BigDecimal) { value = value.add(v, mc) }
+    fun sub(v: BigDecimal) { value = value.subtract(v, mc) }
+    fun mul(v: BigDecimal) { value = value.multiply(v, mc) }
+    fun div(v: BigDecimal) { value = value.divide(v, mc) }
 
-        fun parse(): BigDecimal {
-            var x = term()
-            while (i < s.length) {
-                when (s[i]) {
-                    '+' -> { i++; x = x.add(term(), mc) }
-                    '-' -> { i++; x = x.subtract(term(), mc) }
-                    else -> return x
-                }
-            }
-            return x
-        }
-
-        fun term(): BigDecimal {
-            var x = factor()
-            while (i < s.length) {
-                when (s[i]) {
-                    '×' -> { i++; x = x.multiply(factor(), mc) }
-                    '÷' -> { i++; x = x.divide(factor(), mc) }
-                    '%' -> { i++; x = x.remainder(factor(), mc) }
-                    else -> return x
-                }
-            }
-            return x
-        }
-
-        fun factor(): BigDecimal {
-            val start = i
-            while (i < s.length && (s[i].isDigit() || s[i] == '.')) i++
-            return BigDecimal(s.substring(start, i), mc)
-        }
+    fun display(): String {
+        return value.stripTrailingZeros().toEngineeringString()
     }
 }
